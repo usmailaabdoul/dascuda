3d0473b3611dd8941e364d948ae38964b6df3346
usmailaabdoul <ismaelabdul77@gmail.com>
2023-10-21 12:57:04 +0100

    UI page

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          1: '#4C51C6',
+          2: '#607FF2'
+        },
+        'light': '#E9F4FF',
+        'dark': '#0E0741',
+        'secondary': '#FD830D'
+      },
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
+  },
+  plugins: [],
+}
