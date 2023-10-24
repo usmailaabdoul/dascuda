a2e137b8b7693258a09273a0ce4520b0b839bbf5
usmailaabdoul <ismaelabdul77@gmail.com>
2023-10-24 06:25:40 +0100

    +

--- pages/index.js
+++ pages/index.js
@@ -88,9 +88,6 @@ export async function getStaticProps() {
   // seedStudents()
 
   const students = await prisma.student.findMany({
-    where: {
-      mode: 'insensitive'
-    },
     orderBy: {
       name: 'asc',
       mode: 'insensitive'
